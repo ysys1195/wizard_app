27e64a4e8b0522116c6ec91b125a9c36a3ac22af
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-01-30 19:28:55 +0900

    1ページ目のチェックとform情報保存

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,6 +8,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new
   end
 
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
